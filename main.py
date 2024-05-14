b77ecba601b3669343dad0d4c584f4b44a1764c0
Mohammed DelliHr <mohammedellihr@gmail.com>
2024-05-14 14:54:18 +0200

    Automated commit

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
                 main()
                 continue
             elif text == "":
-                text = "Automated commit"
+                text = "Automated commit with EasyGit"
             Git.run(text)
 
         elif choice == b' ':  # Space key
